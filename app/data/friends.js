ec4dfe4a55bd28ff305becc5332b9851038634a4
Patrick Keane <pkeane92@yahoo.com>
2018-12-20 17:04:50 -0600

    wrote some dummy data in friend.js and wrote in htmlRoute.js file

---
+++ app/data/friends.js
@@ -0,0 +1,62 @@
+var friendArray = [
+
+{
+    "name" : "Bob",
+    "photo" : "../images/Bob.png",
+    "scores" : [
+        2,
+        4,
+        1,
+        3,
+        5,
+        3,
+        2,
+        4,
+        2,
+        3
+    ]
+
+},
+
+{
+    "name" : "Susan",
+    "photo" : "../images/Susan.jpg",
+    "score" : [
+        1,
+        3,
+        5,
+        4,
+        3,
+        2,
+        3,
+        2,
+        1,
+        5
+    ]
+},
+
+{
+    "name" : "Tom",
+    "photo" : "../images/Tom.jpg",
+    "score" : [
+        4,
+        3,
+        3,
+        1,
+        5,
+        2,
+        3,
+        2,
+        4,
+        5
+    ]
+
+},
+]
+
+console.log(friendArray);
+console.log(friendArray[0].name);
+console.log(friendArray[0].scores[0]);
+console.log(friendArray[0].scores[9]);
+console.log(friendArray[1].name);
+console.log(friendArray[2].name);
