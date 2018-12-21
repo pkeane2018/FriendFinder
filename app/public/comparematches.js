a9332d8cc473558608ce391605494b6682c91ccb
Patrick Keane <pkeane92@yahoo.com>
2018-12-21 17:26:00 -0600

    working on compare matches function

---
+++ app/public/comparematches.js
@@ -0,0 +1,66 @@
+var allFriends = [5,2,3,3]; 
+
+var matches = [];
+
+function compareMatches () {
+
+        var x = 0;
+        var y = 0;
+        var tie = false;
+
+        for (i = 1; i < allFriends.length; i++) {
+
+        // if ((allFriends[i] < allFriends[i - 1]) && (allFriends[i] <  allFriends[x])) 
+        
+        if (allFriends[i] <  allFriends[x]) {
+        
+          x = i;
+          console.log("X is " + x);
+
+        }
+
+        else if (allFriends[i] ===  allFriends[x]) {
+
+            console.log(tie);
+            tie = true;
+            y = i;
+            matches.push(y);
+            console.log("Y is " + y);
+            console.log(tie);
+
+        }
+
+        // else {
+
+        //   x = i - 1;
+        //   console.log(x);
+
+        // }
+
+      };
+
+      matches.push(x);
+
+      if (tie === false) {
+
+        console.log("The match position is " + x);
+
+      }
+
+      else if (tie === true) {
+
+        console.log("You had more than one match");
+        console.log("Your matches are in positions ");
+        console.log(matches);
+        
+
+
+      }
+
+      
+
+      // console.log("The lowest number is in position " + x);
+
+      };
+
+      compareMatches();
