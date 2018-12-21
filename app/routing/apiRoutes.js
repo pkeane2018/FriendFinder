fd774b3ee19ec2fd681668ecd085db8004590323
Patrick Keane <pkeane92@yahoo.com>
2018-12-20 22:22:38 -0600

    wrote function to keep survey answers and made post request to successfully send new survey data to friends API

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,16 +2,17 @@ var friendData = require("../data/friends.js");
 
 module.exports = function(app) {
 
-    app.get("/api.friends", function(req, res){
+    app.get("/api/friends", function(req, res){
 
         res.json(friendData)
 
-    })
+    });
+
+    app.post("/api/friends", function(req, res){
+
+        friendData.push(req.body);
+    
+     })
 
 };
 
-app.post("/api.friends", function(req, res){
-
-    friendData.push(req.body);
-
- })
