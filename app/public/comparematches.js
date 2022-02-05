ff6a13ace988638f4e5f85118701c6e50d4add90
pkeane <pkeane92@yahoo.com>
2022-02-05 13:25:45 -0600

    clean up

--- app/public/comparematches.js
+++ app/public/comparematches.js
@@ -1,70 +1,57 @@
-var allFriends = [5,2,3,3]; 
+var allFriends = [5, 2, 3, 3];
 
-function compareMatches () {
+function compareMatches() {
 
-    var matches = [];
+  var matches = [];
 
-        var x = 0;
-        var y = 0;
-        var tie = false;
+  var x = 0;
+  var y = 0;
+  var tie = false;
 
-        for (i = 1; i < allFriends.length; i++) {
+  for (i = 1; i < allFriends.length; i++) {
 
-        // if ((allFriends[i] < allFriends[i - 1]) && (allFriends[i] <  allFriends[x])) 
-        
-        if (allFriends[i] <  allFriends[x]) {
-        
-          x = i;
-          console.log("X is " + x);
+    if (allFriends[i] < allFriends[x]) {
 
-        }
+      x = i;
+      console.log("X is " + x);
 
-        else if (allFriends[i] ===  allFriends[x]) {
+    }
 
-            console.log(tie);
-            tie = true;
-            y = i;
-            matches.push(y);
-            console.log("Y is " + y);
-            console.log(tie);
+    else if (allFriends[i] === allFriends[x]) {
 
-        }
+      console.log(tie);
+      tie = true;
+      y = i;
+      matches.push(y);
+      console.log("Y is " + y);
+      console.log(tie);
 
-        // else {
+    }
 
-        //   x = i - 1;
-        //   console.log(x);
+  };
 
-        // }
+  matches.push(x);
 
-      };
+  if (tie === false) {
 
-      matches.push(x);
+    console.log("The match position is " + x);
 
-      if (tie === false) {
+  }
 
-        console.log("The match position is " + x);
+  else if (tie === true) {
 
-      }
+    console.log("You had more than one match");
+    console.log("Your matches are in positions ");
+    console.log(matches);
 
-      else if (tie === true) {
+    for (i = 0; i < matches.length; i++) {
 
-        console.log("You had more than one match");
-        console.log("Your matches are in positions ");
-        console.log(matches);
-        
-        for (i = 0; i < matches.length; i++) {
+      console.log(data[matches[i]].name);
 
-          console.log(data[matches[i]].name);
-            
-          }
+    }
 
-      }
+  }
 
-      
+};
 
-      // console.log("The lowest number is in position " + x);
-
-      };
-
-      compareMatches();
+compareMatches();
